b5989ce9a2ee15bb5b5cc975e7bc6697f94ca442
e-a-n <ean.director@gmail.com>
2026-03-23 21:35:33 -0700

    messing with return values

--- ownership.rs
+++ ownership.rs
@@ -2,7 +2,7 @@ struct TaskData {
     name: String,
 }
 
-fn example_task(task: &mut TaskData, nummy: i32) {
+fn example_task(task: &mut TaskData, nummy: i32) -> i32 {
     task.name.drain(..);
     task.name.push_str(
         // format allows println! syntax to build strings
@@ -10,11 +10,13 @@ fn example_task(task: &mut TaskData, nummy: i32) {
         &format!("Crazy Dazyyy! {}", nummy)
     );
     println!("Oh shit! {} is a crazy task! {}", task.name, nummy);
+    let result : i32 = nummy * 2;
+    return result;
 }
 
 fn actual_main() {
     let mut task1: TaskData = TaskData{name: String::with_capacity(10)};
-    example_task(&mut task1, 5);
+    println!("Nummy is now {}.", example_task(&mut task1, 5));
     example_task(&mut task1, 9);
 }
 
