92d5294931164ca555db94d26de1ef59cb17b72b
e-a-n <ean.director@gmail.com>
2026-03-23 19:57:15 -0700

    getting comfy with Rust's scoping/ownership rule

--- ownership.rs
+++ ownership.rs
@@ -3,13 +3,21 @@ struct TaskData {
 }
 
 fn example_task(task: &mut TaskData, nummy: i32) {
+    task.name.drain(..);
+    task.name.push_str(
+        // format allows println! syntax to build strings
+        // for usage instead of printing
+        &format!("Crazy Dazyyy! {}", nummy)
+    );
     println!("Oh shit! {} is a crazy task! {}", task.name, nummy);
 }
 
-fn main() {
+fn actual_main() {
     let mut task1: TaskData = TaskData{name: String::with_capacity(10)};
-    task1.name.drain(..);
-    task1.name.push_str("Aye!!");
     example_task(&mut task1, 5);
     example_task(&mut task1, 9);
+}
+
+fn main() {
+    actual_main();
 }
